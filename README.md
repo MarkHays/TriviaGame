11204097e91aa83e62a6401f216a782ab2ce57e1
MarkHays <mark.hays1991@gmail.com>
2018-10-18 18:13:39 -0700

    saving changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TriviaGame
+trying to resolve a bug in the game. when reset button is hit and it loads the next new question it automatically says time out. then goes to the next question and resumes functionality as normal until the end of the game.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,11 +3,11 @@ $('#start').on('click', function () {
     $('#start').remove();
     game.loadQuestion();
 })
-$(document).on('click', '.answer-button',function(e){
+$(document).on('click', '.answer-button', function (e) {
     game.clicked(e);
 })
 
-$(document).on('click', '#reset', function(){
+$(document).on('click', '#reset', function () {
     game.reset();
 })
 // next we set our questions
@@ -68,7 +68,7 @@ var game = {
         }
     },
     loadQuestion: function () {
-        timer = setInterval(game.countdown,1000);
+        timer = setInterval(game.countdown, 1000);
         $('#subwrapper').html("<h2>TIME LEFT <span id='counter'>30</span> Seconds</h2>");
         $('#subwrapper').append('<h2>' + questions[game.currentQuestion].question + '</h2>');
         for (var i = 0; i < questions[game.currentQuestion].answers.length; i++) {
@@ -85,26 +85,26 @@ var game = {
         clearInterval(timer);
         game.unanswered++;
         $('#subwrapper').html('<h2>OUT OF TIME!</2>');
-        $('#subwrapper').append('<h3>The Correct Answer Was: '+questions[game.currentQuestion].correctAnswer+'</h3>');
-        if(game.currentQuestion==questions.length-1){
-            setTimeout(game.results,3*1000);
+        $('#subwrapper').append('<h3>The Correct Answer Was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
         } else {
-            setTimeout(game.nextQuestion,3*1000);
+            setTimeout(game.nextQuestion, 3 * 1000);
         }
     },
     results: function () {
         clearInterval(timer);
         $('#subwrapper').html("<h2>Your Results</h2>");
-        $('#subwrapper').append("<h3>Correct: "+game.correct+"</h3>");
-        $('#subwrapper').append("<h3>Incorrect: "+game.incorrect+"</h3>");
-        $('#subwrapper').append("<h3>Unanswered: "+game.unanswered+"</h3>");
+        $('#subwrapper').append("<h3>Correct: " + game.correct + "</h3>");
+        $('#subwrapper').append("<h3>Incorrect: " + game.incorrect + "</h3>");
+        $('#subwrapper').append("<h3>Unanswered: " + game.unanswered + "</h3>");
         $('#subwrapper').append("<button id='reset'>RESET</button>")
     },
     clicked: function (e) {
         clearInterval(timer);
-        if($(e.target).data("name")==questions[game.currentQuestion].correctAnswer){
+        if ($(e.target).data("name") == questions[game.currentQuestion].correctAnswer) {
             game.answeredCorrectly();
-            
+
         }
         else {
             game.answeredIncorrectly();
@@ -115,10 +115,12 @@ var game = {
         clearInterval(timer);
         game.correct++;
         $('#subwrapper').html('<h2>YOU GOT IT RIGHT!</h2>');
-        if(game.currentQuestion==questions.length-1){
-            setTimeout(game.results,3*1000);
+        //$('#subwrapper').append('<img class="loadergif" src="../assets/apple.gif" width="" height="">');
+
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
         } else {
-            setTimeout(game.nextQuestion,3*1000);
+            setTimeout(game.nextQuestion, 3 * 1000);
         }
     },
     answeredIncorrectly: function () {
@@ -126,11 +128,11 @@ var game = {
         clearInterval(timer);
         game.incorrect++;
         $('#subwrapper').html('<h2>YOU GOT IT WRONG!</h2>');
-        $('#subwrapper').append('<h3>The Correct Answer Was: '+questions[game.currentQuestion].correctAnswer+'</h3>');
-        if(game.currentQuestion==questions.length-1){
-            setTimeout(game.results,3*1000);
+        $('#subwrapper').append('<h3>The Correct Answer Was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
         } else {
-            setTimeout(game.nextQuestion,3*1000);
+            setTimeout(game.nextQuestion, 3 * 1000);
         }
     },
     reset: function () {
